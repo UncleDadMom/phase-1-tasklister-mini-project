9c24ca90f640cdec1cb235e8565399cfdc3fdebc
Patrick Wu <Ptrckwu@yahoo.com>
2021-09-06 13:40:19 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener(`submit`, (e) => {
+    e.preventDefault()
+    const inputText = document.getElementById("new-task-description")
+    addToDo(inputText.value)
+  })
 });
+function addToDo(todo){
+  let p = document.createElement(`p`)
+  p.textContent = todo
+  document.querySelector("#tasks").appendChild(p)
+}
